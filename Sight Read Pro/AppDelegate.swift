a4f3c421ad144226494ddbce18d6f2edc248f49a
anthonygabriele <anthonyjoeseph@gmail.com>
2016-07-06 14:24:23 -0400

    Initial Commit

---
+++ Sight Read Pro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sight Read Pro
+//
+//  Created by Anthony Gabriele on 7/6/16.
+//  Copyright © 2016 Anthony Gabriele Company. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sight Read Pro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sight Read Pro
+//
+//  Created by Anthony Gabriele on 7/6/16.
+//  Copyright © 2016 Anthony Gabriele Company. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
